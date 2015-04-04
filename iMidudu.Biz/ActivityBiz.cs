039ac7a86c74b9a02a64b199417d6be69ecdf4fc
wj620645 <872689338@qq.com>
2015-04-04 22:00:13 +0800

    biz

--- iMidudu.Biz/ActivityBiz.cs
+++ iMidudu.Biz/ActivityBiz.cs
@@ -20,4 +20,5 @@ namespace iMidudu.Biz
             }
         }
     }
+   
 }
--- iMidudu.Data/WXUserDAL.cs
+++ iMidudu.Data/WXUserDAL.cs
@@ -7,6 +7,16 @@ namespace iMidudu.Data
 {
     public class WXUserDAL
     {
-
+        public bool ExistsByOpenid(string openid)
+        {
+            
+          var dr = SuuSee.Data.SqlHelper.ExecuteReaderStoredProcedure("WXUser_SelectProcedure",
+                new System.Data.SqlClient.SqlParameter("@OpenId", openid));
+          while (dr.Read())
+          {
+              return true;
+          }
+          return false;
+        }
     }
 }
