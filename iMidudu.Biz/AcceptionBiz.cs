a4d69e2d0e75a86068c54adc481f0edf8e419e92
DamonY <905116842@qq.com>
2015-04-06 01:16:45 +0800

    AcceptionBiz创建

---
+++ iMidudu.Biz/AcceptionBiz.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace iMidudu.Biz
+{
+   public class AcceptionBiz
+    {
+       public void SaveAcception(iMidudu.Model.Acception accept) 
+       {
+           var dal = new iMidudu.Data.AcceptionDAL();
+           var exists = dal.ExistsByScanHistoryId(accept.ScanHistoryId);
+           if (exists)
+           {
+               var oldaccept = dal.SelectById(accept.ScanHistoryId);
+               oldaccept.Address = accept.Address;
+               oldaccept.Mobile = accept.Mobile;
+               oldaccept.UserName = accept.UserName;
+               oldaccept.ValidCode = accept.ValidCode;
+               oldaccept.Remark = accept.Remark;
+               oldaccept.Status = accept.Status;
+               dal.UpdateAcception(oldaccept);
+           }
+           else
+           {
+               dal.InsertAcception(accept);
+           }
+       }
+    }
+}
